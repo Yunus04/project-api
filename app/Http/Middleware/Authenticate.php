a58c79cb95f412b802bd2d7e38f6a101b0838754
Mohamad Yunus <nuhamady88@gmail.com>
2024-10-08 21:04:56 +0700

    :wrench: update configuration

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,45 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-
-class Authenticate extends Middleware
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+class Authenticate
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json([
+                    'statuscode' => 401,
+                    'data' => null,
+                    'message' => 'User not found'
+                ], 401);
+            }
+        } catch (TokenExpiredException $e) {
+            return response()->json([
+                'statuscode' => 401,
+                'data' => null,
+                'message' => 'Token has expired'
+            ], 401);
+        } catch (TokenInvalidException $e) {
+            return response()->json([
+                'statuscode' => 401,
+                'data' => null,
+                'message' => 'Token is invalid'
+            ], 401);
+        } catch (JWTException $e) {
+            return response()->json([
+                'statuscode' => 401,
+                'data' => null,
+                'message' => 'Token not found'
+            ], 401);
         }
+
+        return $next($request);
     }
 }
